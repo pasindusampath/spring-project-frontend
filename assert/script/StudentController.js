bbca4dd18d1bdada33b639c6d87b07a233429711
pasindusampath <pasindub32@gmail.com>
2023-09-11 15:50:54 +0530

    Updated

--- assert/script/StudentController.js
+++ assert/script/StudentController.js
@@ -1,9 +1,16 @@
 class StudentController {
     constructor() {
-        $('#btnSave').click(()=>{
+        $("#studentForm").submit(function(e) {
+            e.preventDefault();
+        });
+
+
+        /*$('#btnSave').click(()=>{
             console.log('Pressed')
             var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Content-Type", "multipart/form-data");
+
+            
 
             var raw = JSON.stringify({
                 "id": "0",
@@ -14,20 +21,101 @@ class StudentController {
                 "address": $('#address').val(),
                 "guardianPhone": $('#gphone').val()
             });
-
+            var formData = new FormData($('#studentForm')[0]);
             var requestOptions = {
                 method: 'POST',
                 headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
+                body: formData
             };
 
-            fetch("http://192.168.8.106:8080/api/v1/student", requestOptions)
+            fetch("http://localhost:8080/api/v1/student", requestOptions)
                 .then(response => response.text())
                 .then(result => console.log(result))
                 .catch(error => console.log('error', error));
+        });*/
+
+        $('#btnSave').click(() => {
+            const fileInput = document.querySelector('#imageUpload');
+            const formData = new FormData();
+
+            formData.append('imageUpload', fileInput.files[0]);
+            formData.append('name', $('#name').val());
+            formData.append('age', $('#age').val());
+            formData.append('phone', $('#phone').val());
+            formData.append('guardianName', $('#guardianName').val());
+            formData.append('address', $('#address').val());
+            formData.append('guardianPhone', $('#guardianPhone').val());
+
+            console.log(formData)
+
+            var requestOptions = {
+                method: 'POST',
+                /*headers: myHeaders,*/
+                body: formData,
+            };
+
+            fetch("http://localhost:8080/api/v1/student", requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result);
+                    ob.getAllStudents();
+                })
+                .catch(error => console.log('error', error));
         });
+
+    }
+
+    getAllStudents(){
+        var requestOptions = {
+            method: 'GET',
+            /*headers: myHeaders,*/
+            /*body: formData,*/
+        };
+
+        fetch("http://localhost:8080/api/v1/student", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                let parse = JSON.parse(result);
+                console.log(parse)
+                $('#studentData').children('tr').remove();
+                $(parse).each(function(index, value){
+                    let tr = $(`<tr>
+                                        <td style="display: none">${value.id}</td>
+                                        <td>${value.name}</td>
+                                        <td>${value.age}</td>
+                                        <td>${value.address}</td>
+                                     </tr>`);
+                    $('#studentData').append(tr);
+                    console.log(tr)
+                    tr.click(()=>{
+                        console.log(value.id)
+                        ob.setDetails(value.id)
+                    })
+                })
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    setDetails(id){
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:8080/api/v1/student/"+id, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+
+                let parse = JSON.parse(result);
+                console.log(parse.name);
+                $('.student_img img').eq(0).attr('src','data:image/png;base64,'+parse.imageData);
+                $('.cover').css('display','flex');
+                $('.studentDetails').css('display','flex');
+            })
+            .catch(error => console.log('error', error));
+
+
     }
 
 }
-new StudentController();
+let ob =new StudentController();
